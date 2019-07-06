6c8045415ddf1126b6953c0202f13a41c21b8699
Micaela Bray <micaela.bray@gmail.com>
2019-07-05 19:44:55 -0500

    This commit adds in the requirements for npm packages

---
+++ liri.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+
+// Require Inquirer
+var inquirer = require("inquirer");
+
+// Require Axios
+var axios = require("axios");
+
+// Require Moment
+var moment = require("moment");
+
+// Add in keys.js and require it
+require("dotenv").config();
+var keys = require("./keys.js");
+
+
